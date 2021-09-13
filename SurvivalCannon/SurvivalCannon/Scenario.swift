2926752620479f036f2287704eb6da4bea6b6482
João Brentano <joaobrentano.br@gmail.com>
2021-09-13 15:35:50 -0300

    Menu principal

--- SurvivalCannon/SurvivalCannon/Scenario.swift
+++ SurvivalCannon/SurvivalCannon/Scenario.swift
@@ -12,10 +12,14 @@ class Scenario: SKScene {
     
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        let scenario = SKSpriteNode(imageNamed: "scenario")
-        scenario.position = CGPoint(x: 0, y: 0)
-        scenario.size = CGSize(width: self.view!.scene!.size.width, height: self.view!.scene!.size.height)
-        self.addChild(scenario)
+//        let scenario = SKSpriteNode(imageNamed: "scenario")
+//        scenario.position = CGPoint(x: 0, y: 0)
+//        scenario.size = CGSize(width: self.view!.scene!.size.width, height: self.view!.scene!.size.height)
+//        self.addChild(scenario)
+        let mainMenu = MainMenu()
+        mainMenu.position = CGPoint(x: 0, y: 0)
+        mainMenu.size = CGSize(width: self.view!.scene!.size.width, height: self.view!.scene!.size.height)
+        self.addChild(mainMenu)
     }
     
 }
