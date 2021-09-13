d978a17bd0509d2902ae72194ef18e4cbf701b84
lccxm <>
2021-09-13 14:07:57 -0300

    Cannon Model

---
+++ SurvivalCannon/SurvivalCannon/Models/Cannon.swift
@@ -0,0 +1,22 @@
+//
+//  Cannon.swift
+//  SurvivalCannon
+//
+//  Created by Lucca Molon on 13/09/21.
+//
+
+import Foundation
+import SpriteKit
+
+class Cannon {
+    var health: Int
+    var node: SKSpriteNode
+    
+    
+    init() {
+        health = 100
+        let texture = SKTexture(imageNamed: "Cannon")
+        node = SKSpriteNode(texture: texture)
+        node.physicsBody = SKPhysicsBody(texture: texture, size: CGSize(width: node.size.width, height: node.size.height))
+    }
+}
